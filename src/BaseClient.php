d5fbf7433f130345e028c9dbc93972d5ee494e26
Usman Zahid <developerusman@yahoo.com>
2025-08-12 23:14:23 +0500

    Added the base client

--- src/BaseClient.php
+++ src/BaseClient.php
@@ -1,5 +1,55 @@
 <?php
 
-class BaseClient {
+namespace Usman\N8n;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class BaseClient {
+    protected string $baseUrl;
+    protected string $apiKey;
+    protected Client $http;
+
+    public function __construct(string $baseUrl, string $apiKey) {
+        $this->baseUrl = rtrim($baseUrl, '/');
+        $this->apiKey = $apiKey;
+
+        $this->http = new Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $this->apiKey
+            ]
+        ]);
+    }
+
+    protected function get(string $endpoint, array $query = []) {
+        return $this->request('GET', $endpoint, ['query' => $query]);
+    }
+
+    protected function post(string $endpoint, array $data = []) {
+        return $this->request('POST', $endpoint, ['json' => $data]);
+    }
+
+    protected function put(string $endpoint, array $data = []) {
+        return $this->request('PUT', $endpoint, ['json' => $data]);
+    }
+
+    protected function delete(string $endpoint) {
+        return $this->request('DELETE', $endpoint);
+    }
+
+    private function request(string $method, string $endpoint, array $options = []) {
+        try {
+            $response = $this->http->request($method, $endpoint, $options);
+            return json_decode((string) $response->getBody(), true);
+        } catch (RequestException $e) {
+            throw new \RuntimeException(
+                $e->getResponse()
+                    ? $e->getResponse()->getBody()->getContents()
+                    :$e->getMessage()
+            );
+        }
+    }
 }
--- src/N8nClient.php
+++ src/N8nClient.php
@@ -1,5 +1,7 @@
 <?php
 
-class N8nClient {
+namespace Usman\N8n;
 
+class N8nClient {
+    
 }
