3bb5fe8a5bbeafe439d11498be5b258b3494aaa0
Usman Zahid <developerusman@yahoo.com>
2025-08-13 22:50:17 +0500

    Tags client updates with docs and more

--- src/Clients/TagsClient.php
+++ src/Clients/TagsClient.php
@@ -5,23 +5,63 @@ namespace Usman\N8n\Clients;
 use Usman\N8n\BaseClient;
 
 class TagsClient extends BaseClient {
-    public function list(): array {
-        return $this->get('/tags');
+    /**
+     * Retrieve all tags
+     *
+     * @param int|null $limit
+     * @param string|null $cursor
+     * @return array
+     */
+    public function list(?int $limit = 100, ?string $cursor = null): array {
+        $query = [];
+        if ($limit!==null) {
+            $query['limit'] = $limit;
+        }
+        if ($cursor!==null) {
+            $query['cursor'] = $cursor;
+        }
+
+        return $this->get('/tags', $query);
     }
 
-    public function getById(int $id): array {
+    /**
+     * Retrieves a tag by ID
+     *
+     * @param string $id
+     * @return array
+     */
+    public function getById(string $id): array {
         return $this->get("/tags/{$id}");
     }
 
+    /**
+     * Creates a tag
+     *
+     * @param string $name
+     * @return array
+     */
     public function create(string $name): array {
         return $this->post('/tags', ['name' => $name]);
     }
 
-    public function update(int $id, string $name): array {
+    /**
+     * Updates a tag by ID
+     *
+     * @param string $id
+     * @param string $name
+     * @return array
+     */
+    public function update(string $id, string $name): array {
         return $this->put("/tags/{$id}", ['name' => $name]);
     }
 
-    public function deleteById(int $id): array {
+    /**
+     * Deletes a tag by ID
+     *
+     * @param string $id
+     * @return array
+     */
+    public function deleteById(string $id): array {
         return $this->delete("/tags/{$id}");
     }
 }
