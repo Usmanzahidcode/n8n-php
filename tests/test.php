a02de2b04851b33567bc3e631d833dc65981a06a
Usman Zahid <developerusman@yahoo.com>
2025-09-06 01:09:39 +0500

    Minor changes

--- tests/test.php
+++ tests/test.php
@@ -1,6 +1,6 @@
 <?php
 
-use Usman\N8n\Enums\RequestMethod;
+use Usman\N8n\Enums\WebhookMode;
 use Usman\N8n\N8nClient;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -12,14 +12,13 @@ $dotenv->safeLoad();
 // Write usage code here and test it by running  through the php command
 
 N8nClient::connect(
-    baseUrl: $_ENV['N8N_API_BASE_URL'],
+    baseUrl: $_ENV['N8N_BASE_URL'],
     apiKey: $_ENV['N8N_API_KEY'],
-    webhookBaseUrl: $_ENV['N8N_WEBHOOK_BASE_URL'],
     webhookUsername: $_ENV['N8N_WEBHOOK_USERNAME'],
     webhookPassword: $_ENV['N8N_WEBHOOK_PASSWORD']
 );
 
-$response = N8nClient::webhooks(RequestMethod::Get)
+$response = N8nClient::webhooks(WebhookMode::Test)
     ->withoutBasicAuth()
     ->request("b3b5b1f5-e48a-4d63-8935-f1103cd5f1c6");
 
