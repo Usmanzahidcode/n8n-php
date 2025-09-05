b8e5ca966c084cbfbc4d80f4d2fa7f7ed8f546c3
Usman Zahid <developerusman@yahoo.com>
2025-09-05 17:26:41 +0500

    Hydration added to AuditClient

--- src/Clients/AuditClient.php
+++ src/Clients/AuditClient.php
@@ -3,9 +3,11 @@
 namespace Usman\N8n\Clients;
 
 use Usman\N8n\BaseClient;
+use Usman\N8n\Entities\Audit\Audit;
 
 class AuditClient extends BaseClient {
-    public function generateAudit(array $additionalOptions = []): array {
-        return $this->post('/audit', ['additionalOptions' => $additionalOptions]);
+    public function generateAudit(array $additionalOptions = []): Audit {
+        $response = $this->post('/audit', ['additionalOptions' => $additionalOptions]);
+        return new Audit($response);
     }
 }
