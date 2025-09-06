b6ce26a9641c2f196478da27cfcc9e981d0057c7
Usman Zahid <developerusman@yahoo.com>
2025-09-06 16:07:58 +0500

    User change roel done

--- tests/test.php
+++ tests/test.php
@@ -33,8 +33,8 @@ $users = N8nClient::users();
 //$single = $users->getUser('15f57b56-e334-45a8-a2e3-cdca43418df0', true);
 //var_dump($single);
 
-//$role = $users->changeUserRole('test.user@example.com', 'global:member');
-//var_dump($role);
+$role = $users->changeUserRole('test.user@example.com', 'global:admin');
+var_dump($role);
 //
 //$delete = $users->deleteUser('test.user@example.com');
 //var_dump($delete);
