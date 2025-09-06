e7755285c26b8aa78b0367c8a13fd23f4b94f577
Usman Zahid <developerusman@yahoo.com>
2025-09-06 20:44:32 +0500

    Credentials apis all done with proper documetnation

--- tests/test.php
+++ tests/test.php
@@ -12,5 +12,10 @@ N8nClient::connect(
     apiKey: $_ENV['N8N_API_KEY']
 );
 
-$execution = N8nClient::credentials()->getCredentialSchema('slackApi');
-var_dump($execution);
+$execution = N8nClient::credentials()->deleteCredential("HtmuVykrpaiUfsRV");
+$execution1 = N8nClient::credentials()->deleteCredential("OJ0S2LOQRCCMJvJm");
+
+var_dump(
+    $execution,
+    $execution1
+);
